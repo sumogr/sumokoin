4533972ed29a26791f8cf1f1690d138cb7469c42
sumogr <sumogr@mail.com>
2020-05-23 21:19:39 +0200

    1

--- src/version.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-extern const char* const SUMOKOIN_VERSION_TAG;
-extern const char* const SUMOKOIN_VERSION;
-extern const char* const SUMOKOIN_RELEASE_NAME;
-extern const char* const SUMOKOIN_VERSION_FULL;
-extern const bool SUMOKOIN_VERSION_IS_RELEASE;
