b22b17e49c34d93835d3f931fc2fb002c7d00b42
sumogr <sumogr@mail.com>
2020-08-11 10:11:46 +0200

    1

--- contrib/depends/hosts/darwin.mk
+++ contrib/depends/hosts/darwin.mk
@@ -1,6 +1,6 @@
 OSX_MIN_VERSION=10.12
 OSX_SDK_VERSION=10.15
-LD64_VERSION=530OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
+OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
 LD64_VERSION=530
 darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -B $(host_prefix)/native/bin
 darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++ -B $(host_prefix)/native/bin
