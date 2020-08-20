6d8c6cc0a07cfdcd597f7442e192d7f3c3288a25
sumogr <sumogr@mail.com>
2020-08-20 07:39:01 +0300

    fix for c++17

--- src/crypto/crypto.cpp
+++ src/crypto/crypto.cpp
@@ -32,8 +32,7 @@
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
-#include <cstdlib>
-#include <cstring>
+#include <optional>
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/lock_guard.hpp>
 #include <boost/shared_ptr.hpp>
@@ -42,6 +41,8 @@
 #include "warnings.h"
 #include "crypto.h"
 // #include "hash.h" //included in crypto/crypto.h
+
+#include "cryptonote_config.h"
 
 namespace {
   static void local_abort(const char *msg)
@@ -338,7 +339,7 @@ namespace crypto {
   // This handles use cases for both standard addresses and subaddresses
   //
   // NOTE: This generates old v1 proofs, and is for TESTING ONLY
-  void crypto_ops::generate_tx_proof_v1(const hash &prefix_hash, const public_key &R, const public_key &A, const boost::optional<public_key> &B, const public_key &D, const secret_key &r, signature &sig) {
+  void crypto_ops::generate_tx_proof_v1(const hash &prefix_hash, const public_key &R, const public_key &A, const std::optional<public_key> &B, const public_key &D, const secret_key &r, signature &sig) {
     // sanity check
     ge_p3 R_p3;
     ge_p3 A_p3;
@@ -499,7 +500,7 @@ namespace crypto {
   }
 
   // Verify a proof: either v1 (version == 1) or v2 (version == 2)
-  bool crypto_ops::check_tx_proof(const hash &prefix_hash, const public_key &R, const public_key &A, const boost::optional<public_key> &B, const public_key &D, const signature &sig, const int version) {
+  bool crypto_ops::check_tx_proof(const hash &prefix_hash, const public_key &R, const public_key &A, const std::optional<public_key> &B, const public_key &D, const signature &sig, const int version) {
     // sanity check
     ge_p3 R_p3;
     ge_p3 A_p3;
