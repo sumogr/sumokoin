e0b01f724468416b130d025ba6a5d5734487c115
Sumo Gr <sumogr@mail.com>
2020-11-11 15:48:37 +0200

    Update stack_trace.cpp

--- src/common/stack_trace.cpp
+++ src/common/stack_trace.cpp
@@ -31,7 +31,7 @@
 #else
 #define ELPP_FEATURE_CRASH_LOG 1
 #endif
-#include "thirdparty/easylogging++/easylogging++.h"
+#include "easylogging++/easylogging++.h"
 
 #include <stdexcept>
 #ifdef USE_UNWIND
