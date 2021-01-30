994402dc50d2ea6279b5205c8ed7fb5456359c1d
sumogr <sumogr@mail.com>
2021-01-30 13:25:44 +0200

    updates here and there

--- src/hardforks/hardforks.cpp
+++ src/hardforks/hardforks.cpp
@@ -41,13 +41,13 @@ const hardfork_t mainnet_hard_forks[] = {
   { 6, MAINNET_HARDFORK_V6_HEIGHT, 0, 1537065522 },
   { 7, MAINNET_HARDFORK_V7_HEIGHT, 0, 1555234940 },
   { 8, MAINNET_HARDFORK_V8_HEIGHT, 0, 1555321375 },
-  { 9, 350000, 0, 1574120819 }, // abt 6h47' Nov 19, 2019
-//{ 10, XXXXXX, 0, XXXXXXXXXX }, // CLSAG & EXACT COINBASE
+  { 10, 502257, 0, 1610700171 },
+  { 11, 502617, 0, 1610786571 }
 };
 const size_t num_mainnet_hard_forks = sizeof(mainnet_hard_forks) / sizeof(mainnet_hard_forks[0]);
 const uint64_t mainnet_hard_fork_version_1_till = (uint64_t)-1;
 
-const hardfork_t testnet_hard_forks[] = {
+const hardfork_t testnet_hard_forks[] = { 
   { 1, 1, 0, 1482806500 },
   { 2, 5150, 0, 1497181713 },
   { 3, 103580, 0, 1522540800 },
@@ -72,6 +72,6 @@ const hardfork_t stagenet_hard_forks[] = {
   { 7, 130530, 0, 1554465078 },
   { 8, 130560, 0, 1554479506 },
   { 9, 164100, 0, 1572592223 },
-//{ 10, XXXXXX, 0, XXXXXXXXXX }, // CLSAG & EXACT COINBASE     
+//{ 10, XXXXXX, 0, XXXXXXXXXX }, // CLSAG & EXACT COINBASE
 };
 const size_t num_stagenet_hard_forks = sizeof(stagenet_hard_forks) / sizeof(stagenet_hard_forks[0]);
